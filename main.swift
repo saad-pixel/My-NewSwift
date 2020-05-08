26d37eadea8b174b60e40e9d2515024a2311736a
saad-pixel <marva.saad2@gmail.com>
2020-05-08 12:08:59 +0000

    Button color change

--- main.swift
+++ main.swift
@@ -1,12 +1,27 @@
 
-//import UIKIT
+import UIKIT
 
 var cityName: String = "rio di janiro"
 var fastestSpeed: Double = 4.76543
 var numberOfChildren: Int = 4
 var personIsHome: Bool = false
 
+var colorIsGreen = false
+
 print(cityName)
 print(fastestSpeed)
 print(numberOfChildren)
 print(personIsHome)
+
+@IBAction func myButton(_sender : Any) {
+if colorIsGreen {
+   print ("Wow, change to purple")
+   colorIsGreen = false
+}
+else {
+
+  print ("make it purple")
+  coloeIsGreen = true
+}
+}
+}
