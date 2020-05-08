c74c18c380ac410442394a43bc7c62eea83c1ff8
saad-pixel <marva.saad2@gmail.com>
2020-05-08 12:13:35 +0000

    Turn light on off

--- main.swift
+++ main.swift
@@ -7,21 +7,22 @@ var numberOfChildren: Int = 4
 var personIsHome: Bool = false
 
 var colorIsGreen = false
+// coloris green means turn light on because there is a person entered tge room
 
-print(cityName)
-print(fastestSpeed)
-print(numberOfChildren)
-print(personIsHome)
 
 @IBAction func myButton(_sender : Any) {
 if colorIsGreen {
-   print ("Wow, change to purple")
+   print ("Wow, change to purple, turn room light off")
    colorIsGreen = false
+   personIsHome = false
+   print(personIsHome)
 }
 else {
 
-  print ("make it purple")
+  print ("make it purple,turn light on human enterd the room")
   coloeIsGreen = true
+  personIsHome = true
+  print(personIsHome)
 }
 }
 }
