524b9b435367e92acd5d52e8c85b547bfd883bcf
saad-pixel <marva.saad2@gmail.com>
2020-05-08 01:27:38 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+
+
+var cityName: String = "new york"
+var fastestSpeed: Double = 4.76543
+var numberOfChildren: Int = 4
+var personIsHome: Bool = false
+
+print(cityName)
+print(fastestSpeed)
+print(numberOfChildren)
+print(personIsHome)
